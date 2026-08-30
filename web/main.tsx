30ba17538cea64f39b02d39f791431102110fff4
VDeng <8124973+vdeng-ai@users.noreply.github.com>
2026-08-30 12:58:06 +0800

    feat: enable Liquid Glass theme

--- web/main.tsx
+++ web/main.tsx
@@ -1,5 +1,6 @@
 import { render } from "preact";
 import { App } from "./components/App.js";
 import "./styles.css";
+import "./liquid-glass.css";
 
 render(<App />, document.getElementById("app")!);
