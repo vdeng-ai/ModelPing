360c9627af9f07ae5e6ca2054538cf65af65015d
VDeng <8124973+vdeng-ai@users.noreply.github.com>
2026-08-30 13:01:08 +0800

    feat: load Liquid Glass detail styles

--- web/main.tsx
+++ web/main.tsx
@@ -2,5 +2,6 @@ import { render } from "preact";
 import { App } from "./components/App.js";
 import "./styles.css";
 import "./liquid-glass.css";
+import "./liquid-glass-detail.css";
 
 render(<App />, document.getElementById("app")!);
