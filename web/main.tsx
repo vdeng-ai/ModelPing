782467a4ed232e1dbaf95ebef9aee48298cd0504
VDeng <8124973+vdeng-ai@users.noreply.github.com>
2026-08-31 09:13:42 +0800

    Load dedicated ModelPing brand layer

--- web/main.tsx
+++ web/main.tsx
@@ -5,5 +5,6 @@ import "./design-system/tokens.css";
 import "./design-system/materials.css";
 import "./design-system/motion.css";
 import "./design-system/components.css";
+import "./design-system/brand.css";
 
 render(<App />, document.getElementById("app")!);
